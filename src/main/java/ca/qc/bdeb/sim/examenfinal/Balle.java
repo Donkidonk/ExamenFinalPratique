10eef9ea3054a7294231733b848dacc87044f39b
6309244 <6309244@bdeb.qc.ca>
2025-12-12 15:34:47 -0500

    ELLE A PEUR DES CHATS

--- src/main/java/ca/qc/bdeb/sim/examenfinal/Balle.java
+++ src/main/java/ca/qc/bdeb/sim/examenfinal/Balle.java
@@ -10,6 +10,27 @@ public class Balle extends ObjetDuJeu{
 
 
 
+
+
+
+
+
+
+
+    public boolean collision(Raquette autre){
+        return !(this.getDroite() < autre.getGauche() ||
+                this.getBas() < autre.getHaut() ||
+                autre.getDroite() < this.getGauche() ||
+                autre.getBas() < this.getHaut());
+
+
+    }
+
+    public void rebondir() {
+        vx= -vx;
+        vy= Math.random()*360-180; //DEMANDER A CHAT
+
+    }
     @Override
 
     public void draw(GraphicsContext context) {
